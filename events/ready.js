3000aae1e1e18bae8a989b1eb2e995fc1bd93fea
Kapinrezxx <rxd6944@gmail.com>
2024-05-14 20:43:55 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -43,11 +43,7 @@ console.log('\x1b[36m%s\x1b[0m', '|    ❌ Commands Failed To Load!');
 
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
-const serverCount = client.guilds.cache.size;
-setInterval(() => client.user.setActivity({ 
-  name:`Kapin Rezx`, 
-  type: ActivityType.Listening }), 10000);
-client.errorLog = config.errorLog
+const statusMessages = ["Working under Kapin Rezx"];
   
 }
 
